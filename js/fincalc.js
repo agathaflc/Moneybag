cacb401f8a7fea88b9f81dc73c9c8d2f8141a821
jennyhuang <huangjennny@gmail.com>
2016-04-16 21:15:12 +0800

    sort of finished financialCalc.html

--- js/fincalc.js
+++ js/fincalc.js
@@ -6,14 +6,18 @@
 * While not required, it would be much appreciated if you could link back to http://ravingroo.com
 */
 
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+})
+
 function onlyNumbers(keyinput){
-var input=keyinput.charCode? keyinput.charCode : keyinput.keyCode
-// if key press is not a backspace, a period, or a number, then stop character input
-if ((input != 8) && (input != 46)) {
-if ((input < 48) || (input > 57)) {
-return false;
-}
-}
+	var input=keyinput.charCode? keyinput.charCode : keyinput.keyCode
+	// if key press is not a backspace, a period, or a number, then stop character input
+	if ((input != 8) && (input != 46)) {
+		if ((input < 48) || (input > 57)) {
+			return false;
+		}
+	}
 }
 
 function mySavings()
@@ -23,14 +27,14 @@ document.getElementById('balanceError').innerHTML = '';
 document.getElementById('rateError').innerHTML = '';
 document.getElementById('yearsError').innerHTML = '';*/
 
-document.getElementById('incomeError').innerHTML = '';
-document.getElementById('rentError').innerHTML = '';
-document.getElementById('billsError').innerHTML = '';
-document.getElementById('servicesError').innerHTML = '';
-document.getElementById('groceriesError').innerHTML = '';
-document.getElementById('diningError').innerHTML = '';
-document.getElementById('entertainmentError').innerHTML = '';
-document.getElementById('miscError').innerHTML = '';
+	document.getElementById('incomeError').innerHTML = '';
+	document.getElementById('rentError').innerHTML = '';
+	document.getElementById('billsError').innerHTML = '';
+	document.getElementById('servicesError').innerHTML = '';
+	document.getElementById('groceriesError').innerHTML = '';
+	document.getElementById('diningError').innerHTML = '';
+	document.getElementById('entertainmentError').innerHTML = '';
+	document.getElementById('miscError').innerHTML = '';
 
 
 /*// Form validation checking
@@ -65,40 +69,40 @@ document.getElementById('finalBalance').innerHTML = 'Final Balance: $' + (starti
 }*/
 
 
-if ((document.savingscalc.income.value == null) || (document.savingscalc.income.value.length == 0) || (isNaN(document.savingscalc.income.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('incomeError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.rent.value == null) || (document.savingscalc.rent.value.length == 0) || (isNaN(document.savingscalc.rent.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('rentError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.bills.value == null) || (document.savingscalc.bills.value.length == 0) || (isNaN(document.savingscalc.bills.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('billsError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.services.value == null) || (document.savingscalc.services.value.length == 0) || (isNaN(document.savingscalc.services.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('servicesError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.groceries.value == null) || (document.savingscalc.groceries.value.length == 0) || (isNaN(document.savingscalc.groceries.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('groceriesError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.dining.value == null) || (document.savingscalc.dining.value.length == 0) || (isNaN(document.savingscalc.dining.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('diningError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.entertainment.value == null) || (document.savingscalc.entertainment.value.length == 0) || (isNaN(document.savingscalc.entertainment.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('entertainmentError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else if ((document.savingscalc.misc.value == null) || (document.savingscalc.misc.value.length == 0) || (isNaN(document.savingscalc.misc.value) == true))
-	{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
-	document.getElementById('miscError').innerHTML = 'Numeric value required. Example: 381';
-	}
-else
-{
+	if ((document.savingscalc.income.value == null) || (document.savingscalc.income.value.length == 0) || (isNaN(document.savingscalc.income.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('incomeError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.rent.value == null) || (document.savingscalc.rent.value.length == 0) || (isNaN(document.savingscalc.rent.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('rentError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.bills.value == null) || (document.savingscalc.bills.value.length == 0) || (isNaN(document.savingscalc.bills.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('billsError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.services.value == null) || (document.savingscalc.services.value.length == 0) || (isNaN(document.savingscalc.services.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('servicesError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.groceries.value == null) || (document.savingscalc.groceries.value.length == 0) || (isNaN(document.savingscalc.groceries.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('groceriesError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.dining.value == null) || (document.savingscalc.dining.value.length == 0) || (isNaN(document.savingscalc.dining.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('diningError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.entertainment.value == null) || (document.savingscalc.entertainment.value.length == 0) || (isNaN(document.savingscalc.entertainment.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('entertainmentError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else if ((document.savingscalc.misc.value == null) || (document.savingscalc.misc.value.length == 0) || (isNaN(document.savingscalc.misc.value) == true))
+		{document.getElementById('finalBalance').innerHTML = 'Please enter the mising information.';
+		document.getElementById('miscError').innerHTML = 'Numeric value required. Example: 381';
+		}
+	else
+	{
 /*// Set variables from form data
 var startingbalance = document.savingscalc.balance.value;
 var interestrate = document.savingscalc.rate.value / 100;
@@ -106,55 +110,51 @@ var totalyears = document.savingscalc.years.value;
 // var totalmonths = document.savingscalc.years.value * 12;
 var compoundsperyear = 12;*/
 
-var income = document.savingscalc.income.value;
-var rent = document.savingscalc.rent.value;
-var bills = document.savingscalc.bills.value;
-var services = document.savingscalc.services.value;
-var groceries = document.savingscalc.groceries.value;
-var dining = document.savingscalc.dining.value;
-var entertainment = document.savingscalc.entertainment.value;
-var misc = document.savingscalc.misc.value;
+		var income = document.savingscalc.income.value;
+		var rent = document.savingscalc.rent.value;
+		var bills = document.savingscalc.bills.value;
+		var services = document.savingscalc.services.value;
+		var groceries = document.savingscalc.groceries.value;
+		var dining = document.savingscalc.dining.value;
+		var entertainment = document.savingscalc.entertainment.value;
+		var misc = document.savingscalc.misc.value;
 
-document.getElementById('finalBalance').innerHTML = 'Final Balance: $' + (income - (rent+bills+services+groceries+dining+entertainment+misc));
+		document.getElementById('finalBalance').innerHTML = 'Final Balance: $' + (income - (rent+bills+services+groceries+dining+entertainment+misc));
 
 
 // Calculate final savings balance and display result
 //document.getElementById('finalBalance').innerHTML = 'Final Balance: $' + (startingbalance*Math.pow((1+interestrate/compoundsperyear),(compoundsperyear*totalyears))).toFixed(2);
-}
-
-
+	}
 }
 
 function mySavingsReset()
 {
-// Reset everything to default/null/blank
-document.getElementById('finalBalance').innerHTML = 'Values reset';
+	// Reset everything to default/null/blank
+	document.getElementById('finalBalance').innerHTML = 'Values reset';
 
-/*document.getElementById('balanceError').innerHTML = '';
-document.getElementById('rateError').innerHTML = '';
-document.getElementById('yearsError').innerHTML = '';*/
+	/*document.getElementById('balanceError').innerHTML = '';
+	document.getElementById('rateError').innerHTML = '';
+	document.getElementById('yearsError').innerHTML = '';*/
 
-document.getElementById('incomeError').innerHTML = '';
-document.getElementById('rentError').innerHTML = '';
-document.getElementById('billsError').innerHTML = '';
-document.getElementById('servicesError').innerHTML = '';
-document.getElementById('groceriesError').innerHTML = '';
-document.getElementById('diningError').innerHTML = '';
-document.getElementById('entertainmentError').innerHTML = '';
-document.getElementById('miscError').innerHTML = '';
+	document.getElementById('incomeError').innerHTML = '';
+	document.getElementById('rentError').innerHTML = '';
+	document.getElementById('billsError').innerHTML = '';
+	document.getElementById('servicesError').innerHTML = '';
+	document.getElementById('groceriesError').innerHTML = '';
+	document.getElementById('diningError').innerHTML = '';
+	document.getElementById('entertainmentError').innerHTML = '';
+	document.getElementById('miscError').innerHTML = '';
 
-/*document.savingscalc.balance.value = null;
-document.savingscalc.rate.value = null;
-document.savingscalc.years.value = null;*/
+	/*document.savingscalc.balance.value = null;
+	document.savingscalc.rate.value = null;
+	document.savingscalc.years.value = null;*/
 
-document.savingscalc.income.value = null;
-document.savingscalc.rent.value = null;
-document.savingscalc.bills.value = null;
-document.savingscalc.services.value = null;
-document.savingscalc.groceries.value = null;
-document.savingscalc.dining.value = null;
-document.savingscalc.entertainment.value = null;
-document.savingscalc.misc.value = null;
-
-
+	document.savingscalc.income.value = null;
+	document.savingscalc.rent.value = null;
+	document.savingscalc.bills.value = null;
+	document.savingscalc.services.value = null;
+	document.savingscalc.groceries.value = null;
+	document.savingscalc.dining.value = null;
+	document.savingscalc.entertainment.value = null;
+	document.savingscalc.misc.value = null;
 }
